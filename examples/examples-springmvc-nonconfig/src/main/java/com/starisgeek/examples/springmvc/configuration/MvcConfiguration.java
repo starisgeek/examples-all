e3eee4841bcabd7d92a80928bb33b4fa1576f82b
cloud_wanghao <wanghao_oo@126.com>
2018-09-10 14:38:06 +0800

    修改configureViewResolvers方法

--- examples/examples-springmvc-nonconfig/src/main/java/com/starisgeek/examples/springmvc/configuration/MvcConfiguration.java
+++ examples/examples-springmvc-nonconfig/src/main/java/com/starisgeek/examples/springmvc/configuration/MvcConfiguration.java
@@ -3,10 +3,11 @@ package com.starisgeek.examples.springmvc.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 
 @Configuration
 @EnableWebMvc
@@ -24,7 +25,12 @@ public class MvcConfiguration {
 	public static class DefaultWebMvcConfigurer implements WebMvcConfigurer {
 		@Override
 		public void configureViewResolvers(ViewResolverRegistry registry) {
-			registry.viewResolver(new InternalResourceViewResolver("/WEB-INF/pages/", ".jsp"));
+			// registry.viewResolver(new
+			// InternalResourceViewResolver("/WEB-INF/pages/", ".jsp"));
+			// 创建一个InternalResourceViewResolver实例，并设置prefix和suffex
+			registry.jsp("/WEB-INF/pages/", ".jsp");
+			// 创建ContentNegotiatingViewResolver实例
+			// registry.enableContentNegotiation(new View[0]);
 		}
 	}
 }
