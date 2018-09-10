e658e4c944b83ad49998d60481d2644c1d9b6460
cloud_wanghao <wanghao_oo@126.com>
2018-09-10 14:46:43 +0800

    消除编译告警

--- examples/examples-springmvc-nonconfig/src/main/java/com/starisgeek/examples/springmvc/configuration/MvcConfiguration.java
+++ examples/examples-springmvc-nonconfig/src/main/java/com/starisgeek/examples/springmvc/configuration/MvcConfiguration.java
@@ -22,7 +22,7 @@ public class MvcConfiguration {
 	 */
 	public static class DefaultWebMvcConfigurer implements WebMvcConfigurer {
 		@Override
-		public void configureViewResolvers(ViewResolverRegistry registry) {
+		public void configureViewResolvers(ViewResolverRegistry registry) { 
 			// 创建一个InternalResourceViewResolver实例，并设置prefix和suffex
 			registry.jsp("/WEB-INF/pages/", ".jsp");
 			// 创建ContentNegotiatingViewResolver实例。enableContentNegotiation方法参数不能传null!!!
