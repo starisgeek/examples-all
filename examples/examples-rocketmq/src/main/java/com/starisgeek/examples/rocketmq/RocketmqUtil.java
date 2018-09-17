9f977329389510cc529cd42092af200ece4bf0a1
cloud_wanghao <wanghao_oo@126.com>
2018-09-17 14:48:30 +0800

    工具类

---
+++ examples/examples-rocketmq/src/main/java/com/starisgeek/examples/rocketmq/RocketmqUtil.java
@@ -0,0 +1,8 @@
+package com.starisgeek.examples.rocketmq;
+
+import java.nio.charset.Charset;
+
+public class RocketmqUtil {
+	public static final Charset UTF8 = Charset.forName("UTF-8");
+	public static final String TOPIC_EXAMPLE = "ExampleTopic";
+}
