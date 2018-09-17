09dab51a684d382af2c3c578f7dec8b187720f77
cloud_wanghao <wanghao_oo@126.com>
2018-09-17 15:37:09 +0800

    增加sleep方法

--- examples/examples-rocketmq/src/main/java/com/starisgeek/examples/rocketmq/RocketmqUtil.java
+++ examples/examples-rocketmq/src/main/java/com/starisgeek/examples/rocketmq/RocketmqUtil.java
@@ -1,8 +1,16 @@
 package com.starisgeek.examples.rocketmq;
 
 import java.nio.charset.Charset;
+import java.util.concurrent.TimeUnit;
 
 public class RocketmqUtil {
 	public static final Charset UTF8 = Charset.forName("UTF-8");
 	public static final String TOPIC_EXAMPLE = "ExampleTopic";
+
+	public static void sleep(long timeout, TimeUnit tu) {
+		try {
+			tu.sleep(timeout);
+		} catch (InterruptedException e) {
+		}
+	}
 }
