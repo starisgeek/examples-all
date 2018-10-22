cd3d817dc3fdcfa6e9f42bd626afff96a69afe92
cloud_wanghao <wanghao_oo@126.com>
2018-10-22 11:21:31 +0800

    增加simpleResponse和simpleJsonResponse

--- examples/examples-springmvc-nonconfig/src/main/java/com/starisgeek/examples/springmvc/controller/ExampleController.java
+++ examples/examples-springmvc-nonconfig/src/main/java/com/starisgeek/examples/springmvc/controller/ExampleController.java
@@ -3,13 +3,41 @@ package com.starisgeek.examples.springmvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/examples/springmvc")
+@RequestMapping("/examples")
 public class ExampleController {
 
 	@GetMapping("/hello")
 	public String hello() {
 		return "hello";
+	}
+
+	@GetMapping("/simpleResponse")
+	@ResponseBody
+	public String simpleResponse() {
+		return "简单的字符串";
+	}
+
+	@GetMapping("/simpleJsonResponse")
+	@ResponseBody
+	public Example simpleJsonResponse() {
+		Example e = new Example();
+		e.setV(79);
+		return e;
+	}
+
+	public static class Example {
+		private int v;
+
+		public int getV() {
+			return v;
+		}
+
+		public void setV(int v) {
+			this.v = v;
+		}
+
 	}
 }
