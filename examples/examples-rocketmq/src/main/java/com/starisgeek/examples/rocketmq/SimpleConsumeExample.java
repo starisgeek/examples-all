e096641e5372e71e69070872c469eace1f398801
cloud_wanghao <wanghao_oo@126.com>
2018-12-08 22:06:11 +0800

    修改类名

---
+++ examples/examples-rocketmq/src/main/java/com/starisgeek/examples/rocketmq/SimpleConsumeExample.java
@@ -0,0 +1,42 @@
+package com.starisgeek.examples.rocketmq;
+
+import java.util.List;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SimpleConsumeExample {
+	private static final Logger logger = LoggerFactory.getLogger(SimpleConsumeExample.class);
+
+	public static void main(String[] args) throws Exception {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"spring-rocketmq-consumer.xml");
+		DefaultMQPushConsumer consumer = context.getBean(DefaultMQPushConsumer.class);
+		// consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+		// consumer.setMessageModel(MessageModel.BROADCASTING);
+		consumer.registerMessageListener(new MessageListenerConcurrently() {
+			@Override
+			public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+					ConsumeConcurrentlyContext context) {
+//				logger.info("[{}] message's size:{}", Thread.currentThread().getName(),
+//						msgs.size());
+//				RocketmqUtil.sleep(5, TimeUnit.SECONDS);
+				for (MessageExt msg : msgs) {
+					logger.info("[{}] Received message:{} from rocketmq broker, queueId:{}",
+							Thread.currentThread().getName(),
+							new String(msg.getBody(), RocketmqUtil.UTF8), msg.getQueueId());
+				}
+				return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+			}
+		});
+		consumer.start();
+		System.in.read();
+		context.close();
+	}
+}
