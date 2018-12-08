d0bef6a978b093393afadf9244a0f4f7fc8c81e7
cloud_wanghao <wanghao_oo@126.com>
2018-12-08 22:19:10 +0800

    设置延迟级别

--- examples/examples-rocketmq/src/main/java/com/starisgeek/examples/rocketmq/SimpleProducerExample.java
+++ examples/examples-rocketmq/src/main/java/com/starisgeek/examples/rocketmq/SimpleProducerExample.java
@@ -17,6 +17,9 @@ public class SimpleProducerExample {
 			for (int i = 0; i < 10; i++) {
 				Message msg = new Message(RocketmqUtil.TOPIC_EXAMPLE,
 						("message_" + (i + 1)).getBytes(RocketmqUtil.UTF8));
+//				RcoketMQ的延时等级为：1s，5s，10s，30s，1m，2m，3m，4m，5m，6m，7m，8m，9m，10m，20m，30m，1h，2h。
+//				level=0，表示不延时。level=1，表示 1 级延时，对应延时 1s。level=2 表示 2 级延时，对应5s，以此类推。
+				msg.setDelayTimeLevel(3);
 				producer.send(msg);
 			}
 			logger.info("Message producer send end.");
